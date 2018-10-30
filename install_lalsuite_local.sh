dc7a07892ee1e785530ba31ff06b1544804472e7
Cameron Mills <joseph.mills@ligo.org>
2018-10-30 15:03:36 +0000

    updated install commands

--- install_lalsuite_local.sh
+++ install_lalsuite_local.sh
@@ -1,2 +1,2 @@
 DIRNAME=phenomhm
-~/"${DIRNAME}"/.local/bin/jhbuild -f ~/"${DIRNAME}"/.config/jhbuildrc --no-interact build lalsuite -n
+~/"${DIRNAME}"/.local/bin/jhbuild -f ~/"${DIRNAME}"/.config/jhbuildrc --no-interact build -n lalsuite 
